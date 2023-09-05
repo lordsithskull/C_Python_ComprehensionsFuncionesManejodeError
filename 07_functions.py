a5dc541673a541db7f24bec5787f6d5a697a5618
lordsithskull <74837884+lordsithskull@users.noreply.github.com>
2023-09-05 09:14:38 +0000

    se completa clase de funciones

---
+++ 07_functions.py
@@ -0,0 +1,19 @@
+print('hola')
+
+def my_print(text):
+  print('This is my print')
+  print(text * 2)
+
+my_print('Hola ')
+
+a = 10
+b = 90
+
+c = a + b
+
+def suma(a, b):
+  my_print(a + b)
+
+suma(1, 5) # 6
+suma(10, 4) # 14
+
